43be02c8ea8e8aa55ecc7dd6cafbf2024ca87f8f
lvsong77 <392759128@qq.com>
2020-04-09 20:05:29 +0800

    add tabbar

--- src/views/home/Home.vue
+++ src/views/home/Home.vue
@@ -1,15 +1,18 @@
 <template>
   <div class="home">
     initial done.
+    <tab-bar>tabbar</tab-bar>
   </div>
 </template>
 
 <script>
 
+import TabBar from 'components/common/tabbar/TabBar'
+
 export default {
   name: 'Home',
   components: {
-
+    TabBar
   }
 }
 </script>
