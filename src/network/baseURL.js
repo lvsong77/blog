1c8ef451b5b878c3bf886475a73590b51823cca2
lvsong77 <392759128@qq.com>
2020-05-03 22:20:06 +0800

    delete baseURL

--- src/network/baseURL.js
+++
@@ -1,2 +0,0 @@
-export let cloudMusicPath = 'https://www.mxnzp.com/api'
-export let hitokotoPath = 'https://api.imjad.cn/hitokoto/'
--- src/network/cloudMusic.js
+++ src/network/cloudMusic.js
@@ -2,9 +2,10 @@ import { cloudMusicRequest } from './request'
 
 export function getCloudMusicData() {
   return cloudMusicRequest({
-    url: '/music/order/list',
+    url: '/login/cellphone',
     params: {
-
+      phone: 13281169071,
+      password: 'Lv3Song1'
     },
   })
 }
